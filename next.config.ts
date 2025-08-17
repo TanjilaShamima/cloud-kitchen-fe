ae8c1f6f6243217dfcd49b2a61c2f3caec76a878
Tanjila Shamima <tanjila.cse.diu@gmail.com>
2025-08-17 14:49:23 +0600

    fix issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
+    unoptimized: true,
     domains: [
       'images.unsplash.com',
       'cdn-icons-png.flaticon.com'
